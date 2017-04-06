05ddd1a0bfb27d2c3ce8e51e874bba633987644f
spitrola <spitrola27@gmail.com>
2017-04-06 11:32:10 -0600

    added images

--- app/assets/javascripts/angular/controllers/contentCtrl.js
+++ app/assets/javascripts/angular/controllers/contentCtrl.js
@@ -1,4 +1,11 @@
 angular.module('myapp')
-.controller('contentCtrl', function(){
-
+.controller('contentCtrl', function($scope){
+  $scope.punches = [
+    {title: 'Jab', image:'jab.png'},
+    {title: 'Cross', image:'cross.png'},
+    {title: 'Left Hook', image:'left_hook.jpg'},
+    {title: 'Right Hook', image:'right_hook.png'},
+    {title: 'Left Uppercut', image:'left_uppercut.png'},
+    {title: 'Right Uppercut', image:'right_uppercut.png'}
+  ];
 });
