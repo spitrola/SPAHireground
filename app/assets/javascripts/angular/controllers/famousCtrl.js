449fdfdd24e41e0272c60944869c01a2fa8c5a6d
spitrola <spitrola27@gmail.com>
2017-04-06 12:24:23 -0600

    added content and images for new content

--- app/assets/javascripts/angular/controllers/famousCtrl.js
+++ app/assets/javascripts/angular/controllers/famousCtrl.js
@@ -1,4 +1,11 @@
 angular.module('myapp')
-	.controller('famousCtrl', function(){
-
+	.controller('famousCtrl', function($scope){
+		$scope.boxers = [
+			{name: 'Muhammad Ali', image:'m_ali.jpeg'},
+			{name: 'Mike Tyson', image:'m_tyson.jpg'},
+			{name: 'Joe Louis', image:'j_louis.jpg'},
+			{name: 'Joe Frazier', image:'j_frazier.jpg'},
+			{name: 'Floyd Mayweather', image:'f_may.jpg'},
+			{name: 'Rocky Marciano', image:'rocky_m.jpg'}
+		];
 	});
