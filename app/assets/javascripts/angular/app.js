d5b0c756699320a43779248f13dedf3c864ce0bb
spitrola <spitrola27@gmail.com>
2017-04-05 23:20:50 -0600

    added skeleton files for views and controllers

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,13 +1,25 @@
 angular.module('myapp', ['ui.router', 'templates'])
 
 .config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home/content');
   $stateProvider
 
   .state('home', {
     url: '/home',
     templateUrl: 'angular/views/home.html',
     controller: 'homeCtrl'
+  })
+
+  .state('home.about', {
+    url: '/about',
+    templateUrl: 'angular/views/about.html',
+    controller: 'aboutCtrl'
+  })
+
+  .state('home.content', {
+    url: '/content',
+    templateUrl: 'angular/views/content.html',
+    controller: 'contentCtrl'
   })
 
 });
---
+++ app/assets/javascripts/angular/controllers/contentCtrl.js
@@ -0,0 +1,4 @@
+angular.module('myapp')
+.controller('contentCtrl', function(){
+
+});
